997d74991c3c0b473e834d9e134f0a3120c49495
reshmaabhin <68194633+reshmaabhin@users.noreply.github.com>
2020-10-03 05:26:57 +0530

    Update sketch.js
    Project correction

--- sketch.js
+++ sketch.js
@@ -1,32 +1,58 @@
-//Create variables here
-var dog, happyDog, database, foodS, foodStock;
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
 
-function preload()
-{
-  this.image = loadImage("Dog.png");
-  this.image = loadImage("happydog.png");
-	//load images here
-}
-
-function setup() {
-  createCanvas(500, 500);
-  background(46,139,87);
-  dog = createSprite();
-  dog.addImage("dog.png");
-  foodStock = dataBase.ref('Food');
-  foodStock.on("value",readStock);
-  if(KeyWentDown(Up_Arrow)){
-    writeStock(foodS);
-    dog.addImage(happydog.png);
+function preload(){
+   dogImg=loadImage("Images/Dog.png");
+   dogImg1=loadImage("Images/happy dog.png");
   }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
 }
 
-
-function draw() {  
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
 
   drawSprites();
-  //add styles here
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
 
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
 }
 
 
